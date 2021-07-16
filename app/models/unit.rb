87606264458ce278b63071386c7eae6d8d940944
Davidosky007 <Davidosky1@gmail.com>
2021-07-16 12:42:51 +0100

    Add unit model

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,2 +1,6 @@
 class Unit < ApplicationRecord
+  has_many :measurements
+
+  scope :with_measurements, -> { includes(:measurements) }
+  validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_07_16_041037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "measurements", force: :cascade do |t|
+    t.integer "unit_id"
+    t.integer "user_id"
+    t.float "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "units", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
