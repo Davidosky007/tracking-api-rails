7e02ed18d525012bebdf4bfac978236a2f00620e
Davidosky007 <Davidosky1@gmail.com>
2021-07-16 05:17:48 +0100

    Add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :measurements, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
