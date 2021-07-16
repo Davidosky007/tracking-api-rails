eeb9afb35334af45e9e3d7a5239642dbc3871cd4
Davidosky007 <Davidosky1@gmail.com>
2021-07-16 13:41:40 +0100

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/login', to: 'users#login'
+  get '/auto_login', to: 'users#auto_login'
+  delete '/logout', to: 'users#logout'
+  resources :users, only: [:create]
+  resources :units, only: %i[index show] do
+    resources :measurements, only: %i[create update destroy]
+  end
+  resources :measurements, only: [:index]
 end
