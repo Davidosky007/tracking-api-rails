8c20b95d76638b9a36040dc37bc36cdcfb9a885d
Davidosky007 <Davidosky1@gmail.com>
2021-07-19 12:46:39 +0100

    add shoulda matcher and database cleaner config

--- config/application.rb
+++ config/application.rb
@@ -37,11 +37,11 @@ module TrackingApiRails
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.force_ssl = true unless Rails.env.test?
-    config.middleware.insert_before 0, Rack.cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: %i[get post options]
-      end
-    end
+    # config.middleware.insert_before 0, Rack.Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', headers: :any, methods: %i[get post options]
+    #   end
+    # end
   end
 end
