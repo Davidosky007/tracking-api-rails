9521e90258ff05639b8006dba8d1dc3dbc6879af
Davidosky007 <Davidosky1@gmail.com>
2021-07-19 12:55:46 +0100

    Add test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:measurements) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+  end
 end
