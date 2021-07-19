f5662ff772826d6954766e58d5891c1444751211
Davidosky007 <Davidosky1@gmail.com>
2021-07-19 12:52:07 +0100

    Add test for units

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Unit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:measurements) }
+  end
+
+  describe 'validation' do
+    it { should validate_presence_of(:title) }
+  end
 end
