38b70ac2934f6464f899642cacdfddf1e3a809e6
Davidosky007 <Davidosky1@gmail.com>
2021-08-12 23:36:34 +0100

    Add hash for measurement data

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,7 +1,11 @@
 class MeasurementsController < ApplicationController
   def index
     @measurements = current_user.measurements.with_units.created_on
-    render json: { data: @measurements, status: :ok }
+    data = Hash.new { |h, k| h[k] = [] }
+    @measurements.each do |m|
+      data[m.unit.title] << m
+    end
+    render json: { data: data, status: :ok }
   end
 
   def create
