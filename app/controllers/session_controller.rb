a91c1183e467431147e1c15ecba315aadb0767ce
Davidosky007 <Davidosky1@gmail.com>
2021-08-14 12:45:19 +0100

    Fix session params error

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -13,6 +13,6 @@ class SessionController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:name, :password)
   end
 end
