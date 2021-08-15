f115ec78e7bdb92d7208b4a51fee41a30d9ecaa9
Davidosky007 <Davidosky1@gmail.com>
2021-08-15 06:24:13 +0100

    Fix unexpeted end error

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -4,8 +4,8 @@ class MeasurementsController < ApplicationController
     data = Hash.new { |h, k| h[k] = [] }
     json_result = @measurements.pluck(:value, :id, :unit_id, :user_id, :created_at, :updated_at)
     # @measurements.each do |m|
-      data[m.unit.title] << json_result
-    end
+    data[json_result.unit.title] << json_result
+    # end
     render json: { data: data, status: :ok }
   end
 
